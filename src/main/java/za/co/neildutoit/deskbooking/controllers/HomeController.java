a6f353080eea2b41caef272a2a8ac821499a6e7b
Neil du Toit <neil@snail-mail.co.za>
2024-10-11 10:35:41 +0200

    Fix imports

--- src/main/java/za/co/neildutoit/deskbooking/controllers/HomeController.java
+++ src/main/java/za/co/neildutoit/deskbooking/controllers/HomeController.java
@@ -1,16 +1,11 @@
 package za.co.neildutoit.deskbooking.controllers;
 
-import za.co.neildutoit.deskbooking.dto.DeskDto;
-import za.co.neildutoit.deskbooking.service.DeskService;
-import za.co.neildutoit.deskbooking.util.ThymeleafUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
+import za.co.neildutoit.deskbooking.service.DeskService;
 
 @Controller
 @RequiredArgsConstructor
